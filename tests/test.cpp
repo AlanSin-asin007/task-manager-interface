7dbb3c40d4f8dff6f58bb6f0933825c489109aca
PyroRift <asin007@ucr.edu>
2023-05-20 16:33:51 -0700

    Test parameterized constructors

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,34 +5,64 @@
 
 using namespace std;
 
-TEST(personDefaultConstructor, testName) {
-    Person* testPersonName = new Person();
+TEST(personDefaultConstructor, defaultName) {
+    Person* personDefaultName = new Person();
 
-    EXPECT_EQ("", testPersonName->getName());
+    EXPECT_EQ("", personDefaultName->getName());
 }
 
-TEST(personDefaultConstructor, testEmail) {
-    Person* testPersonEmail = new Person();
+TEST(personDefaultConstructor, defaultEmail) {
+    Person* personDefaultEmail = new Person();
 
-    EXPECT_EQ("", testPersonEmail->getEmail());
+    EXPECT_EQ("", personDefaultEmail->getEmail());
 }
 
-TEST(personDefaultConstructor, testPassword) {
-    Person* testPersonPassword = new Person();
+TEST(personDefaultConstructor, defaultPassword) {
+    Person* personDefaultPassword = new Person();
 
-    EXPECT_EQ("", testPersonPassword->getPassword());
+    EXPECT_EQ("", personDefaultPassword->getPassword());
 }
 
-TEST(personDefaultConstructor, testTasks) {
-    Person* testPersonTasks = new Person();
+TEST(personDefaultConstructor, defaultTasks) {
+    Person* personDefaultTasks = new Person();
 
-    EXPECT_EQ("", testPersonTasks->getTasks());
+    EXPECT_EQ("", personDefaultTasks->getTasks());
 }
 
-TEST(personDefaultConstructor, testFriends) {
-    Person* testPersonFriends = new Person();
+TEST(personDefaultConstructor, defaultFriends) {
+    Person* personDefaultFriends = new Person();
 
-    EXPECT_EQ("", testPersonFriends->getFriends());
+    EXPECT_EQ("", personDefaultFriends->getFriends());
+}
+
+TEST(personParameterizedConstructor, parameterizedName) {
+    Person* personParameterizedName = new Person("testName", "testEmail", "testPassword", "testTasks", "testFriends");
+
+    EXPECT_EQ("testName", personParameterizedName->getName());
+}
+
+TEST(personParameterizedConstructor, parameterizedEmail) {
+    Person* personParameterizedEmail = new Person("testName", "testEmail", "testPassword", "testTasks", "testFriends");
+
+    EXPECT_EQ("testEmail", personParameterizedEmail->getEmail());
+}
+
+TEST(personParameterizedConstructor, parameterizedPassword) {
+    Person* personParameterizedPassword = new Person("testName", "testEmail", "testPassword", "testTasks", "testFriends");
+
+    EXPECT_EQ("testPassword", personParameterizedPassword->getPassword());
+}
+
+TEST(personParameterizedConstructor, parameterizedTasks) {
+    Person* personParameterizedTasks = new Person("testName", "testEmail", "testPassword", "testTasks", "testFriends");
+
+    EXPECT_EQ("testTasks", personParameterizedTasks->getTasks());
+}
+
+TEST(personParameterizedConstructor, parameterizedFriends) {
+    Person* personParameterizedFriends = new Person("testName", "testEmail", "testPassword", "testTasks", "testFriends");
+
+    EXPECT_EQ("testFriends", personParameterizedFriends->getFriends());
 }
 
 int main(int argc, char **argv) {
