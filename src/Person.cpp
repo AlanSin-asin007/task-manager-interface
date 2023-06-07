7ef960ec05a5b922daec22eb39e86538b9db0f2d
PyroRift <asin007@ucr.edu>
2023-06-06 20:42:21 -0700

    Convert cout to throw statements

--- src/Person.cpp
+++ src/Person.cpp
@@ -59,7 +59,7 @@ void Person::setFriends(Person& newFriends) {
     this->friends.push_back(newFriends);
 }
 
-void Person::signUp(const string newName, const string newEmail, const string newPassword) {
+void Person::signUp(const string& newName, const string& newEmail, const string& newPassword) {
     //check database if name/email is currently in use
     if(checkNameRequirements(newName) && checkEmailRequirements(newEmail) && checkPasswordRequirements(newPassword)) {
         this->name = newName;
@@ -70,18 +70,16 @@ void Person::signUp(const string newName, const string newEmail, const string ne
 
 //5-15 char limit
 //only alphabetical
-bool Person::checkNameRequirements(const string newName) const {
+bool Person::checkNameRequirements(const string& newName) const {
     //check char limit
     if(newName.length() < 5 || newName.length() > 15) {
-        cout << "Invalid Name: 5-15 character limit" << endl;
-        return false;
+        throw runtime_error("Invalid Name: 5-15 character limit");
     }
 
     //check if alphabetical
     for(unsigned i = 0; i < newName.length(); ++i) {
         if(newName.at(i) < 'A' || newName.at(i) > 'z' || (newName.at(i) > 'Z' && newName.at(i) < 'a')) {
-            cout << "Invalid Name: Must contain only letters" << endl;
-            return false;
+            throw runtime_error("Invalid Name: Must contain only letters");
         }
     }
     return true;
@@ -96,17 +94,15 @@ bool Person::checkNameRequirements(const string newName) const {
 //case 2: "@" somewhere in the str before the "."
 //case 3: text after "@" but before "."
 //case 4: text after "."
-bool Person::checkEmailRequirements(const string newEmail) const {
+bool Person::checkEmailRequirements(const string& newEmail) const {
     //check char limit is greater than 5 and less than 257
     if(newEmail.length() < 6 || newEmail.length() > 256) {
-        cout << "Invalid Email: 6-257 character limit" << endl;
-        return false;
+        throw runtime_error("Invalid Email: 6-257 character limit");
     }
 
     //check if first char is a letter
     if(newEmail.at(0) < 'A' || newEmail.at(0) > 'z' || (newEmail.at(0) > 'Z' && newEmail.at(0) < 'a')) {
-        cout << "Invalid Email: First character must be a letter" << endl;
-        return false;
+        throw runtime_error("Invalid Email: First character must be a letter");
     }
 
     //check for "@" and "."
@@ -128,30 +124,26 @@ bool Person::checkEmailRequirements(const string newEmail) const {
         //     return false;
         // }
         else if(newEmail.at(i) < 'A' || newEmail.at(i) > 'z' || (newEmail.at(i) > 'Z' && newEmail.at(i) < 'a')) {
-            cout << "Invalid Email: Must contain only letters" << endl;
-            return false;
+            throw runtime_error("Invalid Email: Must contain only letters");
         }
     }
     //check if there is only one "@" and is before "."
     if(atCounter != 1 || atIndex >= dotIndex - 1) {
-        cout << "Invalid Email: Must contain only one '@' and is before '.'" << endl;
-        return false;
+        throw runtime_error("Invalid Email: Must contain only one '@' and is before '.'");
     }
     //check if there is only one "." and is not the last char
     else if(dotCounter != 1 || dotIndex == newEmail.length() - 1) {
-        cout << "Invalid Email: Must contain only one '.' and is not the last character" << endl;
-        return false;
+        throw runtime_error("Invalid Email: Must contain only one '.' and is not the last character");
     }
     return true;
 }
 
 //12-20 char length
 //check if there is at least one letter, special char, and one number
-bool Person::checkPasswordRequirements(const string newPassword) const {
+bool Person::checkPasswordRequirements(const string& newPassword) const {
     //check char limit is greater than 11 and less than 21
     if(newPassword.length() < 12 || newPassword.length() > 20) {
-        cout << "Invalid Password: 12-20 character limit" << endl;
-        return false;
+        throw runtime_error("Invalid Password: 12-20 character limit");
     }
 
     //check if there is at least one letter, special char, and one number
@@ -170,8 +162,7 @@ bool Person::checkPasswordRequirements(const string newPassword) const {
         }
     }
     if(letterCounter == 0 || specialCharCounter == 0 || numCounter == 0) {
-        cout << "Invalid Password: Must contain at least one letter, number, and special character" << endl;
-        return false;
+        throw runtime_error("Invalid Password: Must contain at least one letter, number, and special character");
     }
     return true;
 }
