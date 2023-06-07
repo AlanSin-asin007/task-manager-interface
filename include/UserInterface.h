9b03e760d254d8123117012bc008954487983748
Adithya-Iyer1 <aiyer026@ucr.edu>
2023-06-06 17:00:30 -0700

    Made files required for I/O

---
+++ include/UserInterface.h
@@ -0,0 +1,16 @@
+#ifndef _USER_INTERFACE_
+#define _USER_INTERFACE_
+
+#include <iostream>
+#include "Person.h"
+
+
+class UserInterface {
+    public:
+        void displayDashboard();
+        void displayListView();
+
+};
+
+
+#endif
